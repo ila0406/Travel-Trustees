67575befbd4470cf7d41adf4d7e709a525e80171
Elbashadore <joshtibbetts12@gmail.com>
2022-02-05 13:15:42 -0700

    finalized flight api

--- assets/js/script.js
+++ assets/js/script.js
@@ -60,7 +60,6 @@ function placeLookup (event){
     event.preventDefault();
     var searchPlace = place.val();
     var apiUrl = "https://api.flightapi.io/place/" + apiKey + "/" + searchPlace;
-    alert(apiUrl);
     fetch(apiUrl)
         .then(response =>{
             if (response.ok){
@@ -77,11 +76,11 @@ function placeLookup (event){
 // display places that have been requested
 
 function displayAirport(data){
-    for (var i=0; i<data.suggestions.length; i++){
-        var magicKey = data.suggestions[i].magicKey
+    // for (var i=0; i<data.suggestions.length; i++){
+        var magicKey = data.suggestions[0].magicKey
         var country = "UK"
         var apiUrl = "https://api.flightapi.io/nearby/" + apiKey + "?country=" + country + "&token=" + magicKey
-        console.log(apiUrl);
+        // console.log(apiUrl);
         fetch(apiUrl)
         .then(response =>{
             if (response.ok){
@@ -89,16 +88,17 @@ function displayAirport(data){
             }
         })
         .then(data => {
-            var airports = data["Nearby Airports"][0].Code;
-            console.log(airports);
-            locationDisplay.textContent = data;
+            for (var i=0; i<data["Nearby Airports"].length; i++){
+            var localAirport = data["Nearby Airports"][i].Airport;
+            console.log(localAirport);
+            }
         })
-    }
+    // }
 
 }
 
 
-
+// var airports = data["Nearby Airports"][0].Airports;
 
 
 
