3f0f89b610fd83bd87b03fddcb6c4e5b10a50727
David faidley <david.faidley23@gmail.com>
2022-02-04 17:35:18 -0700

    added search card addition to JS

--- searchpage/searchresultsscript.js
+++ searchpage/searchresultsscript.js
@@ -1,1 +1,21 @@
-console.log("Your Results!")
+var resultText = document.querySelector("#result-text");
+var resultContent = document.querySelector("#result-content");
+var searchForm = document.querySelector("#search-form");
+
+function printResults(travelSearch) {
+  
+    var searchResults = document.createElement('div');
+    searchResults.classList.add('card');
+  
+    var searchCard = document.createElement('div');
+    searchCard.classList.add('card-body');
+    searchResults.append(searchCard);
+  
+    var cardTitle = document.createElement('h3');
+    cardTitle.textContent = travelSearch.title;
+  
+    resultBody.append(cardTitle);
+    resultContent.append(searchResults);
+}
+
+printResults(travelSearch);
