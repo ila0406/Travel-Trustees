093c437ea712a86cf4877c4a5b889cf86f264865
Elbashadore <joshtibbetts12@gmail.com>
2022-02-07 20:45:45 -0700

    integrated geocode API

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,11 @@
 // variables
 
 var search = $("#search-submit");
+geocodeApiKey = "a19e123a3b1cf7f00d08b299db07954c";
 apiKey = "6201ca75706cee74ed57a823";
 place = $("#search")
 var locationDisplay = $("#location");
-
-
+locationNameSelector = $("#search");
 console.log("Hello!")
 
 
@@ -13,10 +13,30 @@ console.log("Hello!")
 
 
 // This is the event for when the user clicks on the search button
-$(search).click(placeLookup);
+$(search).click(geocode);
 
 
-// fetch api for country lookup function
+// Geocode API
+function geocode(event){
+    var cityName = locationNameSelector.val();
+    console.log(cityName);
+    limit = "1";
+    var geocodeUrl = "http://api.openweathermap.org/geo/1.0/direct?q=" + cityName + "&" + "limit=" + limit + "&appid=" + geocodeApiKey;
+    console.log(geocodeUrl);
+    event.preventDefault();
+    fetch(geocodeUrl)
+        .then(function(res) {
+            if (res.ok){
+                return res.json();
+            }   
+        })
+        .then(function(data) {
+            console.log(data[0].lat);
+            console.log(data[0].lon);
+        })
+    
+    
+    }
 
 function placeLookup (event){
     event.preventDefault();
@@ -39,9 +59,10 @@ function placeLookup (event){
 // display places that have been requested
 
 function displayAirport(data){
-    for (var i=0; i<data.suggestions.length; i++){
-        var magicKey = data.suggestions[i].magicKey
+    // for (var i=0; i<data.suggestions.length; i++){
+        var magicKey = data.suggestions[0].magicKey
         var country = document.getElementById("options").value;
+        console.log(country);
         var apiUrl = "https://api.flightapi.io/nearby/" + apiKey + "?country=" + country + "&token=" + magicKey
         console.log(apiUrl);
         fetch(apiUrl)
@@ -51,11 +72,11 @@ function displayAirport(data){
             }
         })
         .then(data => {
-            var airports = data["Nearby Airports"][0].Code;
+            var airports = data["Nearby Airports"];
             console.log(airports);
             locationDisplay.textContent = data;
         })
-    }
+    // }
 
 }
 
