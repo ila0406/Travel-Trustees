7a75f32a5789ae674ebbfb6aec1a83d90c52114a
Delilah <ila0406@gmail.com>
2022-02-12 09:57:24 -0700

    made all quotes single quotes

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,15 +6,15 @@ var searchFormEl = document.querySelector('#search-form');
 var searchCard = document.createElement('div');
 var searchBody = document.createElement('ul');
 var forecastCard = document.createElement('div');
-var forecastBody = $("")
+var forecastBody = $('')
 var travelInfoEl = document.querySelector('tbd-content');
 
-var search = $("#search-submit");
-geocodeApiKey = "a19e123a3b1cf7f00d08b299db07954c";
-apiKey = "37ee8ade-ff48-4981-9af3-394163c2c764";
-place = $("#search")
-var locationDisplay = $("#location");
-locationNameSelector = $("#search");
+var search = $('#search-submit');
+geocodeApiKey = 'a19e123a3b1cf7f00d08b299db07954c';
+apiKey = '37ee8ade-ff48-4981-9af3-394163c2c764';
+place = $('#search')
+var locationDisplay = $('#location');
+locationNameSelector = $('#search');
 
 var searchCountry;
 distance = 50;
@@ -34,8 +34,8 @@ $(search).click(displayCovid);
 function geocode(event){
     var cityName = locationNameSelector.val();
     console.log(cityName);
-    limit = "1";
-    var geocodeUrl = "https://api.openweathermap.org/geo/1.0/direct?q=" + cityName + "&" + "limit=" + limit + "&appid=" + geocodeApiKey;
+    limit = '1';
+    var geocodeUrl = 'https://api.openweathermap.org/geo/1.0/direct?q=' + cityName + '&' + 'limit=' + limit + '&appid=' + geocodeApiKey;
     console.log(geocodeUrl);
     event.preventDefault();
     fetch(geocodeUrl)
@@ -62,7 +62,7 @@ function geocode(event){
 function nearbyAirports(data){
     lat = data[0].lat
     lon = data[0].lon
-    airportsUrl = "https://airlabs.co/api/v9/nearby?lat=" + lat + "&lng=" + lon + "&distance=" + distance + "&api_key=" + apiKey;
+    airportsUrl = 'https://airlabs.co/api/v9/nearby?lat=' + lat + '&lng=' + lon + '&distance=' + distance + '&api_key=' + apiKey;
     console.log(airportsUrl);
     fetch(airportsUrl)
     .then(response => {
@@ -72,11 +72,11 @@ function nearbyAirports(data){
     .then(data =>{
         console.log(data.response);
 
-        searchBody.innerHTML = "";
+        searchBody.innerHTML = '';
 
 
         for (i=0; i<5; i++) {
-            var airportName = data.response["airports"][i]["name"]
+            var airportName = data.response['airports'][i]['name']
             var bodyContentEl = document.createElement('li');
             $(bodyContentEl).text(airportName);
             searchBody.append(bodyContentEl);
@@ -96,7 +96,7 @@ function nearbyAirports(data){
 function weatherSearch(data){
     var lat = data[0].lat;
     var lon = data[0].lon;
-    weatherApiUrl = "https://api.openweathermap.org/data/2.5/onecall?" + "lat=" + lat + "&lon=" + lon + "&units=imperial" + "&appid=" + geocodeApiKey;
+    weatherApiUrl = 'https://api.openweathermap.org/data/2.5/onecall?' + 'lat=' + lat + '&lon=' + lon + '&units=imperial' + '&appid=' + geocodeApiKey;
     fetch(weatherApiUrl)
     .then(response =>{
         if(response.ok){
@@ -111,31 +111,31 @@ function weatherSearch(data){
 function forecast(data){
     for (i=0; i<5; i++){
         var cityName = locationNameSelector.val();
-        forecastWicon = data["daily"][i]["weather"][0].icon;
-        var forecastIconUrl = "https://openweathermap.org/img/wn/" + forecastWicon + ".png";
+        forecastWicon = data['daily'][i]['weather'][0].icon;
+        var forecastIconUrl = 'https://openweathermap.org/img/wn/' + forecastWicon + '.png';
 
-        forecastDay = data["daily"][i].dt;
+        forecastDay = data['daily'][i].dt;
 
-        forecastWind = data["daily"][i].wind_speed;
-        forecastTemp =  data["daily"][i].temp.max;
-        forecastHumidity = data["daily"][i].humidity;
+        forecastWind = data['daily'][i].wind_speed;
+        forecastTemp =  data['daily'][i].temp.max;
+        forecastHumidity = data['daily'][i].humidity;
 
-        forecastWiconEl = document.createElement("img");
-        forecastDayEl = document.createElement("p");
-        forecastTempEl = document.createElement("p");
-        forecastWindEl = document.createElement("p");
-        forecastHumidityEl = document.createElement("p");
+        forecastWiconEl = document.createElement('img');
+        forecastDayEl = document.createElement('p');
+        forecastTempEl = document.createElement('p');
+        forecastWindEl = document.createElement('p');
+        forecastHumidityEl = document.createElement('p');
 
-        $(forecastWiconEl).attr("id", "wicon");
-        $(forecastWiconEl).attr("src", forecastIconUrl);
-        $(forecastWiconEl).attr("alt", "weather icon");
+        $(forecastWiconEl).attr('id', 'wicon');
+        $(forecastWiconEl).attr('src', forecastIconUrl);
+        $(forecastWiconEl).attr('alt', 'weather icon');
 
        $(forecastTempEl).text(`Temp ${forecastTemp} F`);
        $(forecastDayEl).text(` ${cityName}`);
        $(forecastWindEl).text(`Wind: ${forecastWind} MPH`);
        $(forecastHumidityEl).text(`Humidity ${forecastHumidity} %`);
 
-        forecastCard.classList.add("forecastCard");
+        forecastCard.classList.add('forecastCard');
         forecastCard.append(forecastWiconEl);
         forecastCard.append(forecastDayEl);
         forecastCard.append(forecastTempEl);
@@ -143,7 +143,7 @@ function forecast(data){
         forecastCard.append(forecastHumidityEl);
 
        forecastBody.append(forecastCard);
-       console.log(data["daily"][i])
+       console.log(data['daily'][i])
     }
 }
 
@@ -159,7 +159,7 @@ function displayCovid(data){
     var casePerMillion = '';
     var todayCases = '';
 
-    var covidCard = document.createElement("div")
+    var covidCard = document.createElement('div')
 
     fetch(queryCovidURL)
         .then(function (res)   {
@@ -168,17 +168,17 @@ function displayCovid(data){
     .then(function (data) {
         console.log(data);
         console.log('Output list of countries + covid cases')
-        searchBody.innerHTML = "";
+        searchBody.innerHTML = '';
 
         if (searchCountry == 'US'){
-            population = data[212]["population"];
-            casePerMillion = data[212]["casesPerOneMillion"];
-            todayCases = data[212]["todayCases"];
+            population = data[212]['population'];
+            casePerMillion = data[212]['casesPerOneMillion'];
+            todayCases = data[212]['todayCases'];
 
 
-           var populationEl = document.createElement("p");
-           var casePerMillionEl = document.createElement("p");
-           var todayCasesEl = document.createElement("p");
+           var populationEl = document.createElement('p');
+           var casePerMillionEl = document.createElement('p');
+           var todayCasesEl = document.createElement('p');
 
             $(populationEl).text(`Population: ${population}`);
             $(casePerMillionEl).text(`Cases Per Million People: ${casePerMillion}`);
@@ -191,13 +191,13 @@ function displayCovid(data){
           
         }
         else if (searchCountry == 'GB'){
-            population = data[211]["population"];
-            casePerMillion = data[211]["casesPerOneMillion"];
-            todayCases = data[211]["todayCases"];
+            population = data[211]['population'];
+            casePerMillion = data[211]['casesPerOneMillion'];
+            todayCases = data[211]['todayCases'];
 
-            var populationEl = document.createElement("p");
-            var casePerMillionEl = document.createElement("p");
-            var todayCasesEl = document.createElement("p");
+            var populationEl = document.createElement('p');
+            var casePerMillionEl = document.createElement('p');
+            var todayCasesEl = document.createElement('p');
  
              $(populationEl).text(`Population: ${population}`);
              $(casePerMillionEl).text(`Cases Per Million People: ${casePerMillion}`);
@@ -210,13 +210,13 @@ function displayCovid(data){
         
         }
         else if (searchCountry == 'NZ'){
-            population = data[146]["population"];
-            casePerMillion = data[146]["casesPerOneMillion"];
-            todayCases = data[146]["todayCases"];
+            population = data[146]['population'];
+            casePerMillion = data[146]['casesPerOneMillion'];
+            todayCases = data[146]['todayCases'];
 
-            var populationEl = document.createElement("p");
-            var casePerMillionEl = document.createElement("p");
-            var todayCasesEl = document.createElement("p");
+            var populationEl = document.createElement('p');
+            var casePerMillionEl = document.createElement('p');
+            var todayCasesEl = document.createElement('p');
  
              $(populationEl).text(`Population: ${population}`);
              $(casePerMillionEl).text(`Cases Per Million People: ${casePerMillion}`);
@@ -229,13 +229,13 @@ function displayCovid(data){
 
         }
         else if (searchCountry == 'CA'){
-            population = data[35]["population"];
-            casePerMillion = data[35]["casesPerOneMillion"];
-            todayCases = data[35]["todayCases"];
+            population = data[35]['population'];
+            casePerMillion = data[35]['casesPerOneMillion'];
+            todayCases = data[35]['todayCases'];
 
-            var populationEl = document.createElement("p");
-            var casePerMillionEl = document.createElement("p");
-            var todayCasesEl = document.createElement("p");
+            var populationEl = document.createElement('p');
+            var casePerMillionEl = document.createElement('p');
+            var todayCasesEl = document.createElement('p');
  
              $(populationEl).text(`Population: ${population}`);
              $(casePerMillionEl).text(`Cases Per Million People: ${casePerMillion}`);
@@ -248,13 +248,13 @@ function displayCovid(data){
 
         }
         else if (searchCountry == 'MX'){
-            population = data[132]["population"];
-            casePerMillion = data[132]["casesPerOneMillion"];
-            todayCases = data[132]["todayCases"];
+            population = data[132]['population'];
+            casePerMillion = data[132]['casesPerOneMillion'];
+            todayCases = data[132]['todayCases'];
 
-            var populationEl = document.createElement("p");
-            var casePerMillionEl = document.createElement("p");
-            var todayCasesEl = document.createElement("p");
+            var populationEl = document.createElement('p');
+            var casePerMillionEl = document.createElement('p');
+            var todayCasesEl = document.createElement('p');
  
              $(populationEl).text(`Population: ${population}`);
              $(casePerMillionEl).text(`Cases Per Million People: ${casePerMillion}`);
@@ -281,7 +281,7 @@ travelInfoEl.innerHTML=''
     var travelInfoURL = 'https://www.travel-advisory.info/api?countrycode=US';
     var travelInfo = '';
 
-    var travelCard = document.createElement("div")
+    var travelCard = document.createElement('div')
 
     fetch(travelInfoURL)
         .then(function (res)   {
@@ -289,13 +289,13 @@ travelInfoEl.innerHTML=''
         })
     .then(function (data) {
         console.log(data);
-        // searchBody.innerHTML = "";
+        // searchBody.innerHTML = '';
 
         // if (searchCountry == 'US'){
         //     // travelInfo = ;
 
 
-        //    var travelEl = document.createElement("p");
+        //    var travelEl = document.createElement('p');
 
         //     $(travelEl).text(`Population: ${travelInfo}`);
 
